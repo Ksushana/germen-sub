feab0de98137737ff4a747b3660bf05391da60f6
Ksenia Balashova <utyatka@gmail.com>
2020-11-02 15:03:44 +0100

    add new slider

--- build/js/script.js
+++ build/js/script.js
@@ -9289,7 +9289,7 @@ var Tabs = function(options) {
     return;
   }
 
-  var mySwiper = new Swiper('.hero__slider-container', {
+  var mySwiper = new Swiper('.hero__slider-container-1', {
     slidesPerView: 1,
     speed: 400,
     spaceBetween: 10,
@@ -9301,6 +9301,40 @@ var Tabs = function(options) {
       el: '.hero__slider-pagination',
       type: 'bullets',
     },
+  });
+
+  var mySwiper = new Swiper('.hero__slider-container-2', {
+    slidesPerView: 1,
+    speed: 400,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.hero__slider-button-next',
+      prevEl: '.hero__slider-button-prev',
+    },
+    pagination: {
+      el: '.hero__slider-pagination',
+      type: 'bullets',
+    },
+  });
+
+
+})();
+
+(function () {
+  var mono = document.querySelector(".subscribe-page__form-type--mono");
+  var compose = document.querySelector(".subscribe-page__form-type--compose");
+  var monoSlider = document.querySelector(".hero__slider-container-1");
+  var composeSlider = document.querySelector(".hero__slider-container-2");
+
+
+  mono.addEventListener("click", function() {
+    monoSlider.classList.remove("visually-hidden");
+    composeSlider.classList.add("visually-hidden");
+  });
+
+  compose.addEventListener("click", function() {
+    monoSlider.classList.add("visually-hidden");
+    composeSlider.classList.remove("visually-hidden");
   });
 })();
 'use strict';
--- src/js/modules/slider-hero.js
+++ src/js/modules/slider-hero.js
@@ -6,7 +6,7 @@
     return;
   }
 
-  var mySwiper = new Swiper('.hero__slider-container', {
+  var mySwiper = new Swiper('.hero__slider-container-1', {
     slidesPerView: 1,
     speed: 400,
     spaceBetween: 10,
@@ -18,5 +18,39 @@
       el: '.hero__slider-pagination',
       type: 'bullets',
     },
+  });
+
+  var mySwiper = new Swiper('.hero__slider-container-2', {
+    slidesPerView: 1,
+    speed: 400,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.hero__slider-button-next',
+      prevEl: '.hero__slider-button-prev',
+    },
+    pagination: {
+      el: '.hero__slider-pagination',
+      type: 'bullets',
+    },
+  });
+
+
+})();
+
+(function () {
+  var mono = document.querySelector(".subscribe-page__form-type--mono");
+  var compose = document.querySelector(".subscribe-page__form-type--compose");
+  var monoSlider = document.querySelector(".hero__slider-container-1");
+  var composeSlider = document.querySelector(".hero__slider-container-2");
+
+
+  mono.addEventListener("click", function() {
+    monoSlider.classList.remove("visually-hidden");
+    composeSlider.classList.add("visually-hidden");
+  });
+
+  compose.addEventListener("click", function() {
+    monoSlider.classList.add("visually-hidden");
+    composeSlider.classList.remove("visually-hidden");
   });
 })();
