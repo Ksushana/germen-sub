5f17b77a7f9fb18ae999cac7f8109ea5f9ca452e
Ksenia Balashova <utyatka@gmail.com>
2021-01-22 00:53:53 +0100

    hide features

--- build/js/script.js
+++ build/js/script.js
@@ -9371,7 +9371,7 @@ window.changeNumber = () => {
 })();
 'use strict';
 
-window.showDiscription = () => {
+function  showDiscription() {
   var text = document.querySelector('.product-info__discription-text');
   var show = document.querySelector('.product-info__discription-show');
 
@@ -9381,6 +9381,19 @@ window.showDiscription = () => {
 
   show.addEventListener("click", function () {
     text.classList.add("full-text");
+  });
+};
+
+function  showFeatures() {
+  var features = document.querySelector('.product-info__features');
+  var show = document.querySelector('.product-info__features-show');
+
+  if (!features) {
+    return;
+  }
+
+  show.addEventListener("click", function () {
+    features.classList.add("full-text");
   });
 };
 
--- src/js/modules/product-card-discription.js
+++ src/js/modules/product-card-discription.js
@@ -1,6 +1,6 @@
 'use strict';
 
-window.showDiscription = () => {
+function  showDiscription() {
   var text = document.querySelector('.product-info__discription-text');
   var show = document.querySelector('.product-info__discription-show');
 
@@ -10,5 +10,18 @@ window.showDiscription = () => {
 
   show.addEventListener("click", function () {
     text.classList.add("full-text");
+  });
+};
+
+function  showFeatures() {
+  var features = document.querySelector('.product-info__features');
+  var show = document.querySelector('.product-info__features-show');
+
+  if (!features) {
+    return;
+  }
+
+  show.addEventListener("click", function () {
+    features.classList.add("full-text");
   });
 };
